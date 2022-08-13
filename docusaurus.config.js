3bef0a702120153ded9f9970cb6a125113cabdd2
schneidermichael <schneidermichael@outlook.com>
2022-08-13 17:02:40 +0200

    fix: correct baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://schneidermichael.github.io',
-  baseUrl: '/',
+  baseUrl: '/docu/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
